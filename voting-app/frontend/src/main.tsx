3df11c6d9d02719364bc8cc04d352e64f020b1a9
JoelOsagiePaul <pauljoel@punch.agency>
2025-11-21 11:24:20 +0100

    Refactor network selection logic and update .gitignore for environment files

--- voting-app/frontend/src/main.tsx
+++ voting-app/frontend/src/main.tsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
-        <SuiClientProvider defaultNetwork='testnet' networks={networkConfig}>
+        <SuiClientProvider defaultNetwork={getNetwork()} networks={networkConfig}>
           <WalletProvider autoConnect>
             <NavigationProvider>
               <App />
@@ -26,3 +26,17 @@ createRoot(document.getElementById('root')!).render(
     </ThemeProvider>
   </StrictMode>
 )
+
+function getNetwork() {
+  const networks = ["mainnet", "testnet", "devnet"];
+  const network = import.meta.env.VITE_NETWORK;
+
+  console.log("Selecting: " + network);
+
+  if (!networks.includes(network)) {
+    return "testnet";
+  }
+
+  return network;
+}
+
